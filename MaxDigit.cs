0129b70460d5dc7ed424d0608dbf440095626297
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-14 23:18:32 +0600

    Add files via upload

---
+++ MaxDigit.cs
@@ -0,0 +1,11 @@
+﻿int digit = new Random().Next(10,99);
+int digitdouble = digit;
+Console.WriteLine ("Выпавшее число "+digit);
+if (digit/10>digitdouble%10)
+{
+    Console.WriteLine ("Наибольшая Цифра числа "+digit+" является число " +digit/10);
+}
+else 
+{
+    Console.WriteLine ("Наибольшая цифра числа "+digit+" является число "+digitdouble%10);
+}
