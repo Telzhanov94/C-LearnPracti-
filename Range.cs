ca6e873e12c2d3f3853b509a6cc944160d766d3a
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-14 20:00:05 +0600

    Add files via upload

---
+++ Range.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите число для обозначения диапазона");
+int RangeNumber = int.Parse (Console.ReadLine());
+int Count=-RangeNumber;
+while (Count<RangeNumber+1)
+{
+    Console.Write (Count +" ");
+    Count++;
+}
