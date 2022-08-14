ef36f68feec44237d8ba80707253bf67767b7d56
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-14 19:24:11 +0600

    Add files via upload

---
+++ MaxOfTreee.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите первое число ");
+int FirstNumber = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите первое число ");
+int SecondNumber = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите первое число ");
+int ThirdNumber = int.Parse (Console.ReadLine());
+int number=FirstNumber;
+if (SecondNumber>number)
+{
+   number=SecondNumber;
+    }
+if (ThirdNumber>number)
+{
+   number=ThirdNumber;
+    }
+Console.WriteLine ("Максимальное число: "+ number);
