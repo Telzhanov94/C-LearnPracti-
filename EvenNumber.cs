d0f3aa4f2d139df452d7f7739f730fc27c5f3c4f
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-14 19:41:29 +0600

    Add files via upload

---
+++ EvenNumber.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Введите Число: ");
+int Number = int.Parse (Console.ReadLine ());
+if (Number%2==0) {Console.WriteLine (Number +" чётное число");}
+else {Console.WriteLine (Number + " нечётное число");}
