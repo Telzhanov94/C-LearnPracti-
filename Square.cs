66e4ede8d4bddad894f4a0a3ed38f92a227971cd
Temirlan <telzhanov94@gmail.com>
2022-08-14 17:52:00 +0600

    Ready

---
+++ Square.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("ВВедите первое число: ");
+int a = int.Parse (Console.ReadLine());
+Console.WriteLine ("ВВедите второе число: ");
+int b = int.Parse (Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine ("Первое число квадрат второго");
+    }
+if (b==a*a)
+{
+    Console.WriteLine ("Второе число кватрат первого");
+}
+else
+{
+Console.WriteLine ("Числа не являются квадратами друг друга");
+}
