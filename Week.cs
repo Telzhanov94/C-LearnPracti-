b1d7c42ade84c718f9e94f2ce0059893146ac1b9
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-14 17:56:11 +0600

    Add files via upload

---
+++ Week.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите номер дня недели от 1 до 7");
+int number = int.Parse (Console.ReadLine ());
+if (number==1) {Console.WriteLine ("Понедельник он же Monday");}
+if (number==2) {Console.WriteLine ("Вторник он же Tuesday");}
+if (number==3) {Console.WriteLine ("Среда он же Wednesday");}
+if (number==4) {Console.WriteLine ("Черверг он же Thursay");}
+if (number==5) {Console.WriteLine ("Пятница он же Friday");}
+if (number==6) {Console.WriteLine ("Суббота он же Saturday");}
+if (number==7) {Console.WriteLine ("Воскресенье он же Sunday");}
