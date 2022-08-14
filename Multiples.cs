3d14326e5dbfa887bdb548334b9cd6e12058e702
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-14 23:50:02 +0600

    Add files via upload

---
+++ Multiples.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите делимое число");
+int FirstNumber = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите возможное кратное число");
+int SecondNumber = int.Parse(Console.ReadLine());
+if (FirstNumber%SecondNumber==0)
+{
+    Console.WriteLine ("Число ктратное");
+}
+else {Console.WriteLine  ("Число не кратно получаемый остаток равен: " + FirstNumber%SecondNumber );}
