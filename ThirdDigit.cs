1ec4a8da820c7a9099e6700e4c273eb9b3688f97
Telzhanov94 <110475950+Telzhanov94@users.noreply.github.com>
2022-08-15 15:20:56 +0600

    Update ThirdDigit.cs

--- ThirdDigit.cs
+++ ThirdDigit.cs
@@ -1,4 +1,8 @@
 ﻿Console.WriteLine("Введите число");
 String number = Console.ReadLine ();
 char ThirdDigit = number [2];
+Console.WriteLine ("Третье число вашей цифры: " +ThirdDigit);﻿Console.WriteLine("Введите число");
+String number = Console.ReadLine ();
+if (number.Length<3){Console.WriteLine ("Третье число отсутствует");}
+char ThirdDigit = number [2];
 Console.WriteLine ("Третье число вашей цифры: " +ThirdDigit);
